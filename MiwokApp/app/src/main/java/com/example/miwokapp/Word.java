ed62318daa38b45a6102e0722816aa5cbb693822
Shreya Sapale <Shreyasapale@users.noreply.github.com>
2020-10-22 20:04:57 +0530

    Till adding audio files to each word object

--- MiwokApp/app/src/main/java/com/example/miwokapp/Word.java
+++ MiwokApp/app/src/main/java/com/example/miwokapp/Word.java
@@ -3,19 +3,22 @@ package com.example.miwokapp;
 public class Word {
     private String mDefaultTranslation ;
     private String  mMiwokTranslation ;
+    private int mAudioResourceId ;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1 ;
 
 
-    public Word (String defaultTranslation, String miwokTramslation) {
+    public Word (String defaultTranslation, String miwokTramslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTramslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word (String defaultTranslation, String miwokTramslation,int imageResourceId) {
+    public Word (String defaultTranslation, String miwokTramslation,int imageResourceId,int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTramslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     public String getDefaultTranslation () {
